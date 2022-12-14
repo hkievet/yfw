87c69767590b060a85f1477cf867cd356b1a11f2
Hunter Kievet <hkievet@gmail.com>
2022-12-13 20:32:41 -0800

    add basic server for working with the frontend

---
+++ Makefile
@@ -0,0 +1,17 @@
+dev:
+	npm run server:watch
+
+install:
+	npm install -g typescript
+	npm install -g ts-node
+	npm install
+	npm install -g ./
+
+macdeps:
+	brew install ffmpeg
+	brew install yt-dlp
+	pip install git+https://github.com/openai/whisper.git 
+
+upgrademacdeps:	
+	brew upgrade
+	pip install --upgrade --no-deps --force-reinstall git+https://github.com/openai/whisper.git
---
+++ server/index.ts
@@ -0,0 +1,58 @@
+import express from "express";
+import fs from "node:fs";
+import path from "node:path";
+
+const app = express();
+const PORT = 3333;
+
+const downloadedVideosPath = "./process/videos";
+const generatedTranscriptsPath = "./process/output";
+const trimmedVideosPath = "./process/trimmed";
+
+/**
+ * returns a list of videos that are completed and available for trimming
+ */
+async function getAvailableVideos() {
+  return fs.promises.readdir(downloadedVideosPath);
+}
+
+async function getAvailableTranscriptFileNames() {
+  const files = fs.promises.readdir(generatedTranscriptsPath);
+  const srtFiles = (await files).filter((fileName) => {
+    return path.extname(fileName) == ".srt";
+  });
+  return srtFiles;
+}
+
+async function getTrimmedVideos() {
+  const files = fs.promises.readdir(trimmedVideosPath);
+  return files;
+}
+
+app.get("/trimmed", async (req, res) => {
+  let trimmedVideoFiles = await getTrimmedVideos();
+  res.send(trimmedVideoFiles);
+});
+
+app.get("/transcripts", async (req, res) => {
+  let transcriptFiles = await getAvailableTranscriptFileNames();
+  res.send(transcriptFiles);
+});
+
+app.get("/videos", async (req, res) => {
+  let videos = await getAvailableVideos();
+  res.send(videos);
+});
+
+/**
+ * Endpoint to start downloading a video via a url
+ */
+app.post("/start", async (req, res) => {
+  res.send({ hello: "world" });
+});
+
+app.listen(PORT);
+
+app.get("/", async (req, res) => {
+  res.send({ hello: "world" });
+});
